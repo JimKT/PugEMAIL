18ee524bc8fb3ec61c13c524b1ff89b1baefc45c
Jamie Throup <jamie.throup@hub-mdp.co.uk>
2017-03-31 23:14:30 +0100

    Gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,57 @@
 "use strict";
+const pug = require('gulp-pug');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+const inlineCss = require('gulp-inline-css');
 
-// Require this for use in GULP
-var pug = require('gulp-pug');
-var gulp = require('gulp');
-var replace = require ('gulp-replace');
+const URL = 'http://localhost/pug-email/dist/layout.html';
+const pugDKR = 'src/pug/layout.pug'
 
-// PUG Compile + Pretty
 gulp.task('views', function buildHTML() {
-  return gulp.src('layout.pug')
-  .pipe(pug({
-        pretty: true
-    }))
-  .pipe(gulp.dest(''));
+    return gulp.src(pugDKR)
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('dev', function(){
-  gulp.src(['layout.html'])
-    .pipe(replace('src="', 'src="http://media.laredoute.com/repository/sites/3/en-GB/images/'))
-    .pipe(gulp.dest(''));
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('production', function(){
-  gulp.src(['layout.html'])
-    .pipe(replace('src="', 'src="//media.laredoute.com/repository/sites/3/en-GB/images/'))
-    .pipe(gulp.dest(''));
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-// Default Watch
-gulp.task('default', ['views',], function() {
-  // PUG Watch
- gulp.watch(['*.pug'], ['views']);
+gulp.task('inline', function() {
+    return gulp.src('dist/layout.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('imagemin', function(){
+  gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/images/'))
+});
+
+gulp.task('browser-sync', function() {
+    var files = [
+        '**/*.html',
+        '*.html',
+    ];
+    browserSync.init(files, {
+        proxy: URL,
+    });
+});
+
+gulp.task('default', ['browser-sync'], function() {
+    gulp.watch([pugDKR], ['views', 'inline'])
+    gulp.watch(['./src/scss/*.scss'], ['sass', 'inline']);
+    gulp.watch(['**/*.jpg'], ['imagemin']);
 });
