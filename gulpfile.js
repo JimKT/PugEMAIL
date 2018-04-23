dc8825274449eb589c400fc023c4a833dd723aa1
Jamie Throup <jamiethroup@gmail.com>
2018-04-23 20:04:48 +0100

    Progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 "use strict";
+// Required
 const gulp = require('gulp');
 const runSequence = require('run-sequence');
 const replace = require('gulp-replace');
@@ -7,70 +8,25 @@ const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const prettify = require('gulp-jsbeautifier');
 const autoprefixer = require('gulp-autoprefixer');
 const inlinesource = require('gulp-inline-source');
 const inlineCss = require('gulp-inline-css');
-const prettify = require('gulp-jsbeautifier');
-const litmus = require('gulp-litmus');
-const campaigns = require('./src/config');
-const credentials = require('./src/credentials');
-const week = campaigns.Week;
-const campaign = campaigns.Campaign;
-const tracking = campaigns.Tracking;
-const mid = campaigns.Mid;
-const nc = campaigns.Nc;
-const litmusUser = credentials.LitmusUsername;
-const litmusPass = credentials.LitmusPassword;
-const litmusUrl = credentials.LitmusURL;
-const imgSrc = './src/' + week + campaign + '/img/';
-const styleSrc = './src/css/**/*.scss';
-const distHTML = './dist/' + week + '/' + campaign + '/';
-const campaignName = './src/views/' + week + '/' + campaign;
-const pugIndex = './src/views/' + week + '/' + campaign + '/index.pug';
-// Test to Litmus
-const config = {
-	subject: campaign,
-    username: litmusUser,
-    password: litmusPass,
-    url: litmusUrl,
-    applications: [
-		// Gmail
-        'gmailnew',
-        'ffgmailnew',
-		'chromegmailnew',
-		// Yahoo
-		'yahoo',
-		'ffyahoo',
-		'chromeyahoo',
-		// Outlook
-		'outlookcom',
-		'ffoutlookcom',
-		'chromeoutlookcom',
-		// Outlook ( Desktop )
-		'ol2007',
-		'ol2010',
-		'ol2013',
-		// Android
-		'android4',
-		'androidgmailapp',
-		'iphone5s',
-		'iphone6',
-		'iphone6plus',
-		'iphone6s',
-    ]
-}
-gulp.task('litmus', () =>
-	gulp.src(distHTML + 'index.html')
-		.pipe(litmus(config))
-		.pipe(gulp.dest('dist'))
-);
+
+// Directories 
+const campaignName = 'kin-april';
+const imgSrc = './src/' + campaignName + '/img/';
+const styleSrc = './src/css/main.scss';
+const pugIndex = './src/' + campaignName + '/index.pug';
+const distHTML = './dist/' + campaignName + '/';
+
 // Convert PUG into HTML
 gulp.task('pug', () =>
 	 gulp.src(pugIndex)
 		.pipe(pug({
 			pretty: true
 		}))
-		.pipe(gulp.dest('./dist/' + week + '/' + campaign))
+		.pipe(gulp.dest('./dist/' + campaignName))
 );
 gulp.task('latest', () =>
 	 gulp.src(distHTML + 'index.html')
@@ -81,21 +37,21 @@ gulp.task('latest', () =>
 gulp.task('sass', () => 
 	gulp.src(styleSrc)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./dist/css'))
+		.pipe(gulp.dest('./dist/css/'))
 );
 // Inline CSS
 gulp.task('inline-css', () => 
-	gulp.src('dist/' + week + '/' + campaign + '/index.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('./dist/' + week + '/' + campaign + '/'))
+	gulp.src('./dist/' + campaignName + '/index.html')
+		.pipe(inlinesource())
+        .pipe(gulp.dest('./dist/' + campaignName + '/'))
 );
 // Minify Images
 gulp.task('images', () =>
-	gulp.src('src/W01/J286/img/*')
+	gulp.src(imgSrc)
 	.pipe(imagemin({
 		optimizationLevel: 5
 	}))
-	.pipe(gulp.dest('dist/' + week + '/' + campaign + '/img/'))
+	.pipe(gulp.dest('dist/' + campaignName + '/img/'))
 );
 // Beautify HTML
 gulp.task('beautify', () => 
@@ -105,7 +61,7 @@ gulp.task('beautify', () =>
 );
 // Default ( Runs all Tasks on Watch )
 gulp.task('watch-src', () => 
-	runSequence('pug', 'sass', 'inline-css', 'replace-links', 'beautify', 'latest')
+	runSequence('pug', 'sass', 'inline-css', 'latest', 'beautify')
 );
 gulp.task('watch-js', () =>
 	runSequence('clean-dist','pug','live', 'mobile', 'prettify')
@@ -114,7 +70,7 @@ gulp.task('watch-images', () =>
 	runSequence('clean-dist','pug','live', 'mobile', 'prettify')
 );
 gulp.task('default', function() {
-	console.log('Welcome to PUG Email Creator. The current campaign is ' + week + '/' + campaign),
+	console.log('Welcome to PUG Email Creator. The current campaign is ' + campaignName),
 	gulp.watch([styleSrc], ['watch-src']),
 	gulp.watch([pugIndex], ['watch-src']),
 	gulp.watch([campaignName + '/src/js/**/*'], ['watch-js']),
