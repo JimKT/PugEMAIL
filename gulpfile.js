6093adcae53807f321c35db6135cc57ea5b94fb3
Jamie Throup <jamiethroup@gmail.com>
2019-09-01 07:29:12 +0100

    Updated Package and Cleaned GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,17 @@
 "use strict";
 // Required
 const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const replace = require('gulp-replace');
-const clean = require('gulp-clean');
-const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
-const prettify = require('gulp-jsbeautifier');
-const autoprefixer = require('gulp-autoprefixer');
+const beautify = require('gulp-jsbeautifier');
 const inlinesource = require('gulp-inline-source');
-const inlineCss = require('gulp-inline-css');
 // Directories 
 const campaignName = 'met-quarter';
 const imgSrc = './src/' + campaignName + '/images/';
 const styleSrc = './src/css/main.scss';
 const pugIndex = './src/' + campaignName + '/index.pug';
 const distHTML = './dist/' + campaignName + '/';
-const secret = require('./secret.json');
-
-
- 
-const config = {
-    username: secret.user,
-    password: secret.password,
-    url: secret.url,
-    applications: [
-        'applemail6',
-        'gmailnew',
-        'ffgmailnew',
-        'chromegmailnew',
-        'iphone4s',
-    ]
-}
 
 // Convert PUG into HTML
 gulp.task('pug', () =>
@@ -41,11 +19,11 @@ gulp.task('pug', () =>
 		.pipe(pug({
 			pretty: true
 		}))
+		.pipe(beautify())
 		.pipe(gulp.dest('./dist/' + campaignName))
 );
 gulp.task('latest', () =>
 	 gulp.src(distHTML + 'index.html')
-		.pipe(rename("latest.html"))
 		.pipe(gulp.dest('./dist/'))
 );
 // Compile Sass
@@ -68,27 +46,21 @@ gulp.task('images', () =>
 	}))
 	.pipe(gulp.dest('dist/' + campaignName + '/images/'))
 );
-// Beautify HTML
-gulp.task('beautify', () => 
-	gulp.src(distHTML + 'index.html')
-		.pipe(prettify())
-		.pipe(gulp.dest(distHTML)) 
-);
 // Default ( Runs all Tasks on Watch )
 gulp.task('watch-src', () => 
-	runSequence('pug', 'sass', 'inline-css', 'latest', 'beautify', 'images')
+	runSequence('pug', 'sass', 'inline-css', 'latest', 'images')
 );
 gulp.task('watch-js', () =>
-	runSequence('clean-dist','pug','live', 'mobile', 'prettify', 'images')
+	runSequence('pug', 'images')
 );
 gulp.task('watch-images', () =>
-	runSequence('clean-dist','pug','live', 'mobile', 'prettify', 'images')
+	runSequence('pug', 'images')
 );
 gulp.task('default', function() {
 	console.log('Welcome to PUG Email Creator. The current campaign is ' + campaignName),
-	gulp.watch([styleSrc], ['watch-src']),
-	gulp.watch([pugIndex], ['watch-src']),
-	gulp.watch(['./src/views/**/*.pug'], ['watch-src']),
-	gulp.watch([campaignName + '/src/js/**/*'], ['watch-js']),
-	gulp.watch([campaignName + '/src/img/**/*'], ['watch-images'])
+	gulp.watch([styleSrc], gulp.series('pug', 'sass', 'inline-css', 'latest', 'images')),
+	gulp.watch([pugIndex], gulp.series('pug', 'sass', 'inline-css', 'latest', 'images')),
+	gulp.watch(['./src/views/**/*.pug'], gulp.series('pug', 'sass', 'inline-css', 'latest', 'images')),
+	gulp.watch([campaignName + '/src/js/**/*'], gulp.series('pug', 'images')),
+	gulp.watch([campaignName + '/src/img/**/*'], gulp.series('pug', 'images'))
 });
